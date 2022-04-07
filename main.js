1700301e09d68d8eba921b115a22a1e3f954c83c
Bowo David <davidbowo@gmail.com>
2022-04-07 13:48:40 +0100

    added module entrypoint for brackets

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+define(function(require, exports, module) {
+
+    const DocumentManager = brackets.getModule("document/DocumentManager"),
+
+    InMemoryFile = brackets.getModule("document/InMemoryFile"),
+
+    CommandManager = brackets.getModule("command/CommandManager"),
+
+    LiveDev = brackets.getModule("LiveDevelopment/MultiBrowserImpl/launchers/Launcher"),
+
+    Menus = brackets.getModule("command/Menus"),
 
     WorkspaceManager = brackets.getModule("view/WorkspaceManager"),
     
@@ -18,3 +29,41 @@
     function log(s) {
         console.log("[ReachPlugin] "+s);
     }
+
+    function handleReachPluginExecute() {
+        
+        log("Clicked reach execute!");
+
+        if(!panel.isVisible()) 
+        {
+            panel.show();
+        }
+
+        let currentDoc = DocumentManager.getCurrentDocument();
+
+        if(currentDoc == null){
+            Dialogs.showModalDialog(
+                DefaultDialogs.DIALOG_ID_ERROR, 
+                "Please open a reach file in the editor!"
+            );
+        } else if(currentDoc.file instanceof InMemoryFile){
+            Dialogs.showModalDialog(
+                DefaultDialogs.DIALOG_ID_ERROR, 
+                "Please save the reach file before execution!"
+            );
+        }
+        else{
+            passToReachCompiler(currentDoc.file);
+        }
+
+        // if(panel.isVisible()) 
+        // {
+        //     //panel.hide();
+        //     CommandManager.get(REACH_EXECUTE_BTN)//.setChecked(false);
+        // } else 
+        // {
+        //     panel.show();
+        //     CommandManager.get(REACH_EXECUTE_BTN).setChecked(true);
+        // }
+    }
+});
